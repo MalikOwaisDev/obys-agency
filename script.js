02b3c3d96b520ea0f4aa906102874f3c3ef20440
Owais <owaisoff@gmail.com>
2025-01-16 23:39:17 +0500

    made loader

---
+++ script.js
@@ -0,0 +1,38 @@
+let tl = gsap.timeline();
+
+tl.from('.line h1', {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+
+tl.from('.line h2, #line-part1', {
+  opacity: 0,
+  onStart: () => {
+    let h5Timer = document.querySelector('#line-part1 h5');
+    let grow = 0;
+    setInterval(() => {
+      if (grow < 100) {
+        h5Timer.textContent = grow++;
+      } else {
+        h5Timer.textContent = grow;
+      }
+    }, 35);
+  },
+});
+tl.to('#loader', {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+});
+tl.from('#page1', {
+  delay: 0.1,
+  opacity: 0,
+  y: 1600,
+  duration: 0.5,
+  ease: Power4,
+});
+tl.to('#loader', {
+  display: 'none',
+});
